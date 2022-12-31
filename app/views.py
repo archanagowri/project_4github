d546ec5b29243c8f8383c70921d3a792914ca503
Your Name <you@example.com>
2022-12-31 12:19:55 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def ramya(request):
+    return render(request,'ramya.html')
+
+def ram(request):
+    return render(request,'ram.html')
